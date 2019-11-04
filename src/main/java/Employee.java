75f99b56f1b0b7ad0401fa413b9dacef13f75e35
nerojust <nerojust4@gmail.com>
2019-11-04 15:45:34 +0100

    added dependencies

---
+++ src/main/java/Employee.java
@@ -0,0 +1,12 @@
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employees")
+@EntityListeners(AuditingEntityListener.class)
+public class Employee {
+
+}
